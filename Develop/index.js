b3c6e895b93b76455c3481ef403380ed33bbc47e
Stark <mattstark08@gmail.com>
2023-10-27 20:44:49 -0500

    table of contents and basic questions writting

--- Develop/index.js
+++ Develop/index.js
@@ -6,20 +6,71 @@ const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
+    // Title
     {
         type:"input",
-        message:"what do title",
+        message:"what do title?",
         name:"title"
     },
+    //Description
+    {
+        type:"input",
+        message:"what do description?",
+        name:"description"
+    }, 
+    //Table of Contents
+    {
+        type:"input",
+        message:"what do contents",
+        name:"contents"
+    }, 
+    //Installation
+    {
+        type:"input",
+        message:"what do installation",
+        name:"installation"
+    }, 
+    // Usage
+    {
+        type:"input",
+        message:"what do usage",
+        name:"usage"
+    }, 
+    // Credits
+    {
+        type:"input",
+        message:"what do credits",
+        name:"credits"
+    }, 
+    // License
+    {
+        type:"input",
+        message:"what do license",
+        name:"license"
+    }, 
+    // tests
+    {
+        type:"input",
+        message:"what do tests",
+        name:"tests"
+    },
+    // questions
+    {
+        type:"input",
+        message:"what do questions",
+        name:"questions"
+    },
+
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
     fs.writeFile("READMEexample.md", data, (error) => {
         if (error) {
-            throw new Error(error)
+            throw new Error(error);
         } else {
-            console.log("Success")
+            console.log("Success");
+            console.log(questions);
         }
     })
 }
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,7 +12,39 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+# ${data.title}
+
+## Description:
+  ${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+  ${data.installation}
+
+## Usage
+  ${data.usage}
+
+## Credits
+  ${data.credits}
+
+## License
+  ${data.license}
+
+## Tests
+  ${data.tests}
+
+## Questions
+  ${data.questions}
 
 `;
 }
